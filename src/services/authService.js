a82d49b662d0fe1be1ae22179617eee9faa81f14
Rishanth11 <rishanthsomasundaram.gmail.com>
2026-06-25 21:45:34 +0530

    Deploy attempt 1

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // This must match your Spring Boot port!
-const API_URL = 'http://localhost:8080/api/auth'; 
+const API_URL = 'https://deskmind-3kq3.onrender.com'; 
 
 const authService = {
     login: async (email, password) => {
